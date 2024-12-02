439656e2d852e19b22580db9c66ff67565a85b23
Woonters <woonters@gmail.com>
2024-12-02 01:15:44 +0000

    added a rayon implementation for part 1 and the generator
    rayon severly drops efficiency for part 1 so I have my old linear approach as the default
    Part 2 is a slow and sorted afair, I will look into making a better version asap
    rayon did help with the generator though (slight increases)

--- src/day1.rs
+++ src/day1.rs
@@ -1,28 +1,43 @@
 use std::{collections::HashMap, iter::zip};
 
 use parser::parse;
+use rayon::prelude::*;
 
 pub mod parser {
-    use nom::bytes::complete::tag;
-    use nom::character::complete::i32;
+    // thoughts on nom:
+    // I like the idea of splitting the parser into it's owm mod, it's a part of rust I should get better at and do more since it increases the
+    // quality of code. Getting your head around the parser takes a little time but there are some niceties to it and it is certianly faster than my old
+    // naieve approach. in theory if I were writing a larger library there would be some good benifits, the design is meant to encourage easy readability
+    // for a large portion of the parsing (especially for big parsing tasks)
+    use nom::bytes::complete::take_while;
     use nom::character::complete::multispace0;
+    use nom::character::complete::{i32, newline};
+    use nom::combinator::map_res;
     use nom::multi::separated_list0;
     use nom::sequence::separated_pair;
     use nom::IResult;
+
+    // Instead of using the i32 nom parser you can use this custom one, I found it had a slight degredation on speed but was slightly more consistent
+    // not really worth it
+    pub fn num(input: &str) -> IResult<&str, i32> {
+        map_res(take_while(|c: char| c.is_digit(10)), |raw| {
+            i32::from_str_radix(raw, 10)
+        })(input)
+    }
 
     pub fn parse_pair(input: &str) -> IResult<&str, (i32, i32)> {
         separated_pair(i32, multispace0, i32)(input)
     }
 
     pub fn parse(input: &str) -> IResult<&str, Vec<(i32, i32)>> {
-        separated_list0(tag("\n"), parse_pair)(input)
+        separated_list0(newline, parse_pair)(input)
     }
 }
 
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> (Vec<i32>, Vec<i32>) {
     let p = parse(input).unwrap().1;
-    let (mut left, mut right): (Vec<i32>, Vec<i32>) = p.iter().cloned().unzip();
+    let (mut left, mut right): (Vec<i32>, Vec<i32>) = p.par_iter().cloned().unzip();
     left.sort_unstable();
     right.sort_unstable();
     (left, right)
@@ -31,6 +46,16 @@ pub fn input_generator(input: &str) -> (Vec<i32>, Vec<i32>) {
 #[aoc(day1, part1)]
 pub fn solver_p1(input: &(Vec<i32>, Vec<i32>)) -> i32 {
     zip(input.0.iter(), input.1.iter()).fold(0, |acc, (x, y)| acc + ((x - y).abs()))
+}
+
+#[aoc(day1, part1, rayon)]
+pub fn solver_rayon_p1(input: &(Vec<i32>, Vec<i32>)) -> i32 {
+    input
+        .0
+        .par_iter()
+        .zip(input.1.par_iter())
+        .fold(|| 0_i32, |acc, (x, y)| acc + ((x - y).abs()))
+        .sum::<i32>()
 }
 
 #[aoc(day1, part2)]
